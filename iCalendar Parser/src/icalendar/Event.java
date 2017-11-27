c268c06501883abf0dc5184043a26cbf3ac99490
eamv-awp <awp@eamv.dk>
2017-11-27 11:32:34 +0100

    InitialiseringAfEventMedStartOgDurationPaa1Time1Minut1Sekund tilføjet og testet.

--- iCalendar Parser/src/icalendar/Event.java
+++ iCalendar Parser/src/icalendar/Event.java
@@ -1,15 +1,23 @@
 package icalendar;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Event {
 
 	private LocalDateTime start;
 	private LocalDateTime end;
+	private Duration duration;
 
 	public Event(LocalDateTime start, LocalDateTime end) {
 		this.start = start;
 		this.end = end;
+	}
+
+	public Event(LocalDateTime start, Duration duration) {
+		this.start = start;
+		this.duration = duration;
+		this.end = start.plus(duration);
 	}
 
 	public LocalDateTime getStart() {
@@ -18,6 +26,10 @@ public class Event {
 
 	public LocalDateTime getEnd() {
 		return end;
+	}
+
+	public Duration getDuration() {
+		return duration;
 	}
 
 }
