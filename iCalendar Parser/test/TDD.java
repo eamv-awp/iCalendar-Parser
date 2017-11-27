137370dff364714287b3a001843c5cbc2b6b689e
eamv-awp <awp@eamv.dk>
2017-11-27 11:20:45 +0100

    testInitialiseringAfEventMedStartOgDurationPaa1Time1Minut1Sekund added

--- iCalendar Parser/test/TDD.java
+++ iCalendar Parser/test/TDD.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 import org.junit.Test;
@@ -33,6 +34,22 @@ public class TDD {
 		assertEquals(start, event.getStart());
 		assertEquals(end, event.getEnd());
 		
+		// 4. Tear-down
+	}
+
+	@Test
+	public void testInitialiseringAfEventMedStartOgDurationPaa1Time1Minut1Sekund() {
+		// 3. Set-up + // 2. Execute
+		LocalDateTime start = LocalDateTime.of(2012, 12, 12, 12, 12, 12);
+		Duration duration = Duration.ofHours(1).plusMinutes(1).plusSeconds(1);
+		LocalDateTime end = LocalDateTime.of(2012, 12, 12, 13, 13, 13);
+		Event event = new Event(start, duration);
+		
+		// 1. Assert
+		assertEquals(start, event.getStart());
+		assertEquals(duration, event.getDuration());
+		assertEquals(end, event.getEnd());
+
 		// 4. Tear-down
 	}
 
